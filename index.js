1a8cfb6a38cbea3c116b4740257983b31c503c8f
Chuan-Sheng Tai <Johnson@Chuan-Shengs-MacBook-Air.local>
2017-06-27 14:37:47 -0700

    navbar is now functionable

---
+++ index.js
@@ -0,0 +1,31 @@
+$(function(){
+	$('nav a').click(function () {
+	  var $href = $(this).attr('href');
+	  $('body').stop().animate({
+	    scrollTop: $($href).offset().top - 85
+	  }, 1000);
+	  return false;
+	});
+
+	$(document).scroll(function () {
+		if ($(document).scrollTop() > 100 ) {
+			$('nav').css({'height': '70px', 'opacity': '0.95'});
+		}else if ($(document).scrollTop() < 100) {
+			$('nav').css({'height': '', 'opacity': ''});
+		}
+	  var scrollPos = $(document).scrollTop();
+	  $('nav a').each(function () {
+	    var currLink = $(this);
+	    var refElement = $(currLink.attr("href"));
+	    if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+	      $('nav li').removeClass("active");
+	      if (currLink.attr('href') !== '#header') {
+	      	currLink.parent().addClass("active");
+	      }
+	    }
+	  });
+	  if(scrollPos < $('.about').position().top){
+	  	$('nav li').removeClass("active");
+	  }
+	})
+})
